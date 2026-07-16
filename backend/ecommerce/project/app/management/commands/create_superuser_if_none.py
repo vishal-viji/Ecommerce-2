c76603dc2085da8fb78d0a4523bf1f8001f61902
Vishal S <vishalshanmugam928@gmail.com>
2026-07-16 22:47:30 +0530

    Update create_superuser_if_none.py

--- backend/ecommerce/project/app/management/commands/create_superuser_if_none.py
+++ backend/ecommerce/project/app/management/commands/create_superuser_if_none.py
@@ -1,1 +1,23 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
 
+
+class Command(BaseCommand):
+    help = "Create a superuser from environment variables if one doesn't already exist"
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+        if not username or not password:
+            self.stdout.write('Superuser env vars not set, skipping.')
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(f'Superuser "{username}" already exists, skipping.')
+        else:
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(f'Superuser "{username}" created successfully.')
