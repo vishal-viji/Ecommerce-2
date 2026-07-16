f077e4a20ae5a97dc1e975dc231b666dd377acdf
Vishal S <vishalshanmugam928@gmail.com>
2026-07-16 15:46:33 +0530

    Update settings.py

--- backend/ecommerce/project/project/settings.py
+++ backend/ecommerce/project/project/settings.py
@@ -15,6 +15,7 @@ from django.utils.encoding import force_str
 django.utils.encoding.force_text = force_str
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9i_b=p+yz$&91rs#9u_c*+*%11vu#se3dmf%0@&-tr30ni!0(u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=False,
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [ "ecommerce-2-backend.onrender.com",
     "localhost",
     "127.0.0.1",
@@ -135,10 +136,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
